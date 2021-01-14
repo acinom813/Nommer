645aed615a1e85dcc49971e3940c4afa28f8f133
JP-Grace <gracejohnpaul200@gmail.com>
2021-01-13 19:24:05 -0500

    getRandomMeal() method added; placeholder button added; button listener for #search-button added

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,4 +1,16 @@
 // jQuery ready method
 $(document).ready(function() {
 
+    function getRandomMeal() {
+        $.ajax({
+            url: "https://www.themealdb.com/api/json/v1/1/random.php",
+            method: "GET"
+        }).then(function(response) {
+            console.log(response.meals[0]);
+        });
+    }
+
+    $("#search-button").on("click", function() {
+        getRandomMeal();
+    });
 });
