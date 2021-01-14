7a427c2dc1a55d6d0a99d120592340782d47930d
JP-Grace <gracejohnpaul200@gmail.com>
2021-01-13 20:00:32 -0500

    resolved index.html conflicts

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -10,7 +10,41 @@ $(document).ready(function() {
         });
     }
 
+    function getMealByID(id) {
+
+        var parsedURL = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + id;
+
+        $.ajax({
+            url: parsedURL,
+            method: "GET"
+        }).then(function(response) {
+            console.log(response.meals[0]);
+        });
+    }
+
     $("#search-button").on("click", function() {
+
+        // var userInput = $("#search-bar").val();
+
+        // userInput.trim();
+
+        // if (userInput === "") {
+        //     getRandomMeal();
+        // }
+        // else {
+        //     get
+        // }
+
         getRandomMeal();
+    });
+
+    $.ajax({
+        url: "https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast",
+        method: "GET"
+    }).then(function(response) {
+        console.log(response.meals);
+        console.log(response.meals[0]);
+        var mealID = response.meals[0].idMeal;
+        getMealByID(mealID);
     });
 });
